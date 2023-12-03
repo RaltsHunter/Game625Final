f1556ba61a730e14c6a4151ce8fcb0e6f871bbc9
Watkinhj <117215234+Watkinhj@users.noreply.github.com>
2023-12-03 17:27:37 -0500

    Watkin's Coding Shenanigans

---
+++ Game625_Final/Assets/scripts/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TMP_Text winDisplay;
+    public TMP_Text scoreDisplay;
+    public int hunger = 0;
+    public int healthy = 0;
+    public int junk = 0;
+    public int keys = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //scoreDisplay.text = "Fullness: " + hunger;
+        winDisplay.text = "Find The Keys!";
+
+        /*
+        if (hunger == 100)
+        {
+            scoreDisplay.text = "You're Full!";
+            if (healthy > junk)
+            {
+                winDisplay.text = "You are healthy!";
+            }
+            if (healthy < junk)
+            {
+                winDisplay.text = "You are unhealthy...";
+            }
+            if (healthy == junk)
+            {
+                winDisplay.text = "You ate a balanced meal!";
+            }
+        }
+        if (hunger > 100)
+        {
+            scoreDisplay.text = "You're Full!";
+            winDisplay.text = "You're overeating!";
+        }
+        */
+    }
+}
